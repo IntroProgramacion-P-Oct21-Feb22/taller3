94401beba8d61b92fa05b4b10e1b98789a58cf98
@reroes <reroes799@gmail.com>
2021-10-29 00:29:29 -0500

    ejercicios para taller

---
+++ Ejercicio01/src/ejercicio01/Ejercicio01.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejercicio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Resolver
+        
+        // Crear una variable entera llamada edad
+        // Asignar a la variable edad el valor de 20
+        
+        
+        // Crear una variable double llamada promedio
+        // Asignar a la variable promedio el valor de 10.2
+        
+        
+        // Crear una variable String llamada pais
+        // Asignar a la variable pais el valor de Ecuador
+        
+        
+        
+        System.out.println(edad);
+        System.out.println(promedio);
+        System.out.println(pais);
+        
+        
+        System.out.println("-------Ejemplos de expresiones------------------");
+        
+        double resultado = (1+2+4+3)/5+Math.sqrt(81);
+        System.out.println(resultado);
+        
+        
+    }
+    
+}
